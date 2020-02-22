389a4abbeef4c599f96c835cbbfd0e1531530beb
RodrigoRocaR <61027147+RodrigoRocaR@users.noreply.github.com>
2020-02-22 13:06:13 +0100

    Trabajo de casa

--- P1.py
+++ P1.py
@@ -1,4 +1,27 @@
 import arcade
+import random
+
+
+def dibujar_brillitos_fondo():
+    for i in range(11):
+        coord_x = random.randint(0, 800)
+        coord_y = random.randint(210, 500)  # fondo entre 100 y 550 pero para no chocar con otras cosas pongo -
+        arcade.draw_circle_filled(coord_x, coord_y, 3, arcade.color.WHITE)
+
+
+def dibujar_columnas_altas_fondo():
+    x_coords = [10, 110, 450]
+    for i in x_coords:
+        arcade.draw_lrtb_rectangle_filled(i, i+50, 400, 100, arcade.color.COOL_BLACK)
+        arcade.draw_circle_filled(i+25, 400, 25, arcade.color.COOL_BLACK)
+
+
+def dibujar_columnas_bajas_fondo():
+    x_coords = [60, 500]
+    for i in x_coords:
+        arcade.draw_lrtb_rectangle_filled(i, i + 25, 250, 100, arcade.color.COOL_BLACK)
+        arcade.draw_circle_filled(i + 12.5, 250, 12.5, arcade.color.COOL_BLACK)
+
 
 arcade.open_window(800, 600, "First draw")  # width, height
 arcade.set_background_color(arcade.color.DARK_MIDNIGHT_BLUE)
@@ -12,12 +35,11 @@ arcade.draw_triangle_filled(400, 100, 300, 100, 300, 200, arcade.color.DARK_BLUE
 arcade.draw_triangle_filled(650, 100, 800, 100, 800, 25, arcade.color.DARK_MIDNIGHT_BLUE)  # bot right
 # Decoracion
 arcade.draw_triangle_filled(525, 550, 575, 550, 550, 400, arcade.color.ICEBERG)  # tempano de hielo
-
+arcade.draw_triangle_filled(525, 550, 535, 550, 550, 400, arcade.color.WHITE)
 # Detalles
-arcade.draw_rectangle_filled(535, 475, 125, 10, arcade.color.WHITE, 97.5)
-
-
-
+dibujar_brillitos_fondo()
+dibujar_columnas_altas_fondo()
+dibujar_columnas_bajas_fondo()
 
 arcade.finish_render()
 arcade.run()
